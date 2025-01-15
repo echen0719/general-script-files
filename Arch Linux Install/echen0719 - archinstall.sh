991b6703150f6a5f052314630d2f4d088c6e4dc9
Eric Chen <echen0719@gmail.com>
2025-01-14 19:40:05 -0500

    Update echen0719 - archinstall.sh
    Updated logic, allowed user to type efi and swap size. Updated how xinitrc is set up and network config.

--- Arch Linux Install/echen0719 - archinstall.sh
+++ Arch Linux Install/echen0719 - archinstall.sh
@@ -2,11 +2,6 @@
 
 ## Use for install on entire disk (clean)
 
-efi_size="1G"
-swap_size="2G"
-root_size=
-filesystem=ext4
-
 ## base base-devel linux linux-headers linux-firmware dkms iwd archlinux-keyring sudo nano dosfstools systemd
 ## networkmanager dhcpcd man-db man-pages firefox dolphin lxde prismlauncher ark kate spectacle code obs-studio cheese vlc steam htop partitionmanager neofetch xorg-server xorg xorg-xinit xorg-xauth slim python jdk-openjdk jdk8-openjdk jdk17-openjdk cups pipewire ncdu 7zip git
 
@@ -14,7 +9,7 @@ ip a | grep wlan
 
 read -p "Enter the WIFI SSID: " ssid
 read -p "Enter the WIFI Device:  " w_device
-read -p "Enter the WIFI Pass  " passphrase
+read -p "Enter the WIFI Pass: " passphrase
 
 iwctl --passphrase="$passphrase" station "$w_device" connect "$ssid"
 timedatectl set-ntp true
@@ -22,18 +17,27 @@ timedatectl set-ntp true
 read -p "Enter the device path (i.e. /dev/nvme0n1): " device
 
 boot_dev="${device}1"
-root_dev="${device}2"
-swap_dev="${device}3"
+swap_dev="${device}2"
+root_dev="${device}3"
 
-sfdisk /dev/"$device" << EOF
-boot_dev : start=2048, size=+$efi_size, type=0FC63DAF-8483-4772-8E79-3D69D8477DE4, bootable
-swap_dev : size=+$swap_size, type=0657FD6D-A4AB-43C4-84E5-0933C84B4F4F
-root_dev: type=0FC63DAF-8483-4772-8E79-3D69D8477DE4
+sfdisk --delete $device
+
+root_size=
+filesystem=ext4
+
+read -p "Enter EFI Size (1G):  " efi_size
+read -p "Enter Swap Size (2G):  " swap_size
+
+sfdisk $device << EOF
+    label: gpt
+    $boot_dev : size=1G, type=C12A7328-F81F-11D2-BA4B-00A0C93EC93B
+    $swap_dev : size=2G, type=0657FD6D-A4AB-43C4-84E5-0933C84B4F4F
+    $root_dev : type=0FC63DAF-8483-4772-8E79-3D69D8477DE4
 EOF
 
-mkfs.fat -F 32 "$boot_dev"
-mkfs.ext4 "$root_dev"
+mkfs.vfat -F32 "$boot_dev"
 mkswap "$swap_dev"
+mkfs.ext4 "$root_dev"
 
 mount "$root_dev" /mnt
 mkdir /mnt/boot
@@ -51,11 +55,7 @@ arch-chroot /mnt sed -i "s/#en_US.UTF-8/en_US.UTF-8/g" /etc/locale.gen
 export LANG=en_US.UTF-8
 "archer" > /mnt/etc/hostname
 
-cat > /mnt/etc/hosts <<HOSTS
-127.0.0.1      localhost
-::1            localhost
-127.0.1.1      archer.localdomain     archer
-HOSTS
+echo -e "127.0.0.1      localhost\n::1            localhost\n127.0.1.1      archer.localdomain     archer" > /mnt/etc/hosts
 
 arch-chroot /mnt passwd
 
@@ -70,9 +70,9 @@ arch-chroot /mnt passwd "echen0719"
 
 arch-chroot bootctl install
 
-arch-chroot rm /etc/X11/xinit/xinitrc
-arch-chroot cp xinitrc /etc/X11/xinit/xinitrc
-# I should probably change
+arch-chroot echo -e "#!/bin/sh\n\nuserresources=\$HOME/.Xresources\nusermodmap=\$HOME/.Xmodmap\nsysresources=/etc/X11/xinit/.Xresources\nsysmodmap=/etc/X11/xinit/.Xmodmap\n\n# merge in defaults and keymaps\n\nif [ -f \$sysresources ]; then\n    xrdb -merge \$sysresources\nfi\n\nif [ -f \$sysmodmap ]; then\n    xmodmap \$sysmodmap\nfi\n\nif [ -f "\$userresources" ]; then\n    xrdb -merge "\$userresources"\nfi\n\nif [ -f "\$usermodmap" ]; then\n    xmodmap "\$usermodmap"\nfi\n\nstartlxde" > /etc/X11/xinit/xinitrc
+
+arch-chroot chmod u+x /etc/X11/xinit/xinitrc
 
 # Big thanks to
 
