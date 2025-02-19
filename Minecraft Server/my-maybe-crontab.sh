7386d8c4f8a5cf37a1690779cf2a07dc5dba3464
Eric Chen <echen0719@gmail.com>
2025-02-19 14:41:00 -0500

    Update my-maybe-crontab.sh

--- Minecraft Server/my-maybe-crontab.sh
+++ Minecraft Server/my-maybe-crontab.sh
@@ -5,7 +5,7 @@
 checkFolderSize () {
     size=$(du -s --block-size 1 backups/ | awk '{print $1}') # print $1 seems to print first argument, cool!
     if [ ${size} -gt 3000000000 ]; then # Why these spaces...So confusing...
-        echo $(date +"%m/%d/%Y at %H:%M:%S:") Bro, clean it 💀.
+        echo $(date +"%m/%d/%Y at %H:%M:%S:") Bro, clean it.
     else
         echo $(date +"%m/%d/%Y at %H:%M:%S:") Good -- Has not exceeded 3GB yet.
     fi
