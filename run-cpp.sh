1a3f4bd170125049189881418c045423ad495a7d
Eric Chen <echen0719@gmail.com>
2025-05-16 10:48:47 -0400

    Renamed some files

--- run-cpp.sh
+++ run-cpp.sh
@@ -1,16 +1,26 @@
 #!/bin/bash
 
+# remove .cpp extension
 name="${1%.cpp}"
 
+# compile and run
 g++ $1 -o $name
-./$name
 
+# if an error occurs
+if [[ $? != 0 ]]; then # $? is the error code
+    echo "You either didn't specify a valid file or have an error in your code!"
+    exit
+fi
+
+./$name
 echo
 
+# auto deletion after compiling
 while true; do
-    read -p "Type 'q' to delete the compiled file: " key
-    if [[ "$key" == "q" || "$key" == "Q" ]]; then
-        rm -f "$name"
-        break
+    read -p "Type 'q' to delete the compiled file or any other key to exit: " key
+    if [[ $key == "q" || $key == "Q" ]]; then
+        rm -f $name
+        exit
+    else exit
     fi
 done
