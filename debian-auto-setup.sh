8592939c179ee4b4e83820964efe5f49e0bdb5be
Eric Chen <echen0719@gmail.com>
2025-06-11 12:36:41 -0400

    Update debian-auto-setup.sh
    Added eclipse installer and added function to add a custom profile to konsole.

--- debian-auto-setup.sh
+++ debian-auto-setup.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-debianPackages=("code" "firefox-esr" "flatpak" "libqt5core5a" "libqt5gui5" "libqt5network5" "plasma-discover-backend-flatpak")
+debianPackages=("code" "firefox-esr" "flatpak" "htop" "libqt5core5a" "libqt5gui5" "libqt5network5" "libreoffice" "plasma-workspace-wallpapers" "plasma-discover-backend-flatpak" "vlc")
 
 pipPackages=("aiohttp" "aiohttp_socks" "beautifulsoup4" "matplotlib" "opencv-python" "pandas" "polars" "polars-lts-cpu" "pynput" "requests" "scikit-learn" "scipy" "selenium" "torch")
 
-installDebianPackages() {
+installDebianPackages () {
     for package in "${debianPackages[@]}"; do
         sudo apt install "$package"
         sudo apt update
+        clear
     done
     flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
     reboot
@@ -18,6 +19,7 @@ installPythonPackages () {
     for package in "${pipPackages[@]}"; do # similar to enchanced for loop
         # sudo pip3 install "$package" --break-system-packages # install on root as well
         pip3 install "$package" --break-system-packages
+        clear
     done
 }
 
@@ -28,18 +30,57 @@ githubDesktop () {
     sudo apt update && sudo apt install github-desktop
 }
 
+# self-explainatory
 googleChrome () {
     wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
     sudo dpkg -i google-chrome-stable_current_amd64.deb
-    rm -r google-chrome-stable_current_amd64.deb
+    rm google-chrome-stable_current_amd64.deb
 }
 
+# self-explainatory
 multiMc () {
     wget https://files.multimc.org/downloads/multimc_1.6-1.deb
     sudo dpkg -i multimc_1.6-1.deb
-    rm -r multimc_1.6-1.deb
+    rm multimc_1.6-1.deb
 }
 
+eclipseIDE() {
+    wget https://www.eclipse.org/downloads/download.php?file=/technology/epp/downloads/release/2020-06/R/eclipse-java-2020-06-R-linux-gtk-x86_64.tar.gz
+    tar -xzf eclipse-java-2020-06-R-linux-gtk-x86_64.tar.gz -C ~/.local/share # into the user applications binary folder
+    rm eclipse-java-2020-06-R-linux-gtk-x86_64.tar.gz
+    echo 'alias eclipse="~/.local/share/eclipse/eclipse"' >> ~/.bashrc
+}
 
+konsoleTheme () {
+cat << EOF >> ~/.local/share/konsole/Dark.profile
+[Appearance]
+ColorScheme=Breeze
+Font=Monospace,14,-1,5,50,0,0,0,0,0
 
+[Cursor Options]
+CustomCursorTextColor=255,255,255
+UseCustomCursorColor=false
+
+[General]
+Command=bash
+Name=Dark
+Parent=FALLBACK/
+EOF
+} # really weird indentation
+
+echoAlias () {
+    echo 'alias python="python3"' >> ~/.bashrc
+    echo 'alias javach="sudo update-alternatives --config java"' >> ~/.bashrc
+    echo 'alias reboot="sudo reboot"' >> ~/.bashrc
+    echo 'alias poweroff="sudo poweroff"' >> ~/.bashrc
+    echo 'complete -o default java' >> ~/.bashrc
+}
+
+installDebianPackages
 installPythonPackages
+githubDesktop
+googleChrome
+multiMc
+eclipseIDE
+konsoleTheme
+echoAlias
