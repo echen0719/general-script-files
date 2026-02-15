72cb19d3393ad79fbd7237d8c3a7c3876e2b1be7
Eric Chen <echen0719@gmail.com>
2026-02-14 19:55:26 -0500

    Update findMCServers.sh
    I tested my code for a little bit and realized that testing the first port ranges of 0, 10, 127, and 192 is kind of useless and yields false results.

--- findMCServers.sh
+++ findMCServers.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
 
-while true; do
-    # By the way, $RANDOM is from 0 to 32767 so dividing by 128 does the job of numbers 0-255
-    ip="$((RANDOM/128)).$((RANDOM/128)).$((RANDOM/128)).$((RANDOM/128))"
+# $RANDOM is from 0 to 32767 so dividing by 128 does the job of numbers 0-255
+generateIP() {
+    while true; do
+        o1=$((RANDOM % 256))
+        # avoid the local and the computer reserved IP ranges
+        if [ "$o1" -eq 0 ] || [ "$o1" -eq 10 ] || [ "$o1" -eq 127 ] || [ "$o1" -eq 192 ]; then
+            continue
+        fi; break
+    done
 
+    echo "$o1.$((RANDOM/128)).$((RANDOM/128)).$((RANDOM/128))"
+}
+
+while true; do
+    ip=$(generateIP)
     # when grep returns exit code 0, the for loop breaks
     if nmap -p 25565 $ip -Pn | grep -q "open"; then
         echo "Open port found on $ip"
