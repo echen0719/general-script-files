9e0fe93108ed2f6534c68e708b09772d7f24fd39
Eric Chen <echen0719@gmail.com>
2026-04-16 10:50:20 -0400

    Create deltaMathScore.js
    I wanted to top the leaderboard on one of my assignments so I just made this program. I don't really have much knowledge on async functions and promises in JS so I had to ask AI about that part. Everything else was really simple to implement. It works and now I am on the leaderboard.

---
+++ deltaMathScore.js
@@ -0,0 +1,50 @@
+(async () => {
+    const url = 'https://www.deltamath.com/api/module_data_settings';
+    
+    const payload = {
+        "correct": 1,
+        "sk": "calculusFlashCards",
+        "jwt": "", // key found by network requests
+        "operation": "update_record",
+        "history": {
+        },
+        "teacher_id": 0, // both also found in network requests
+        "teachercode": 0,
+    };
+
+    let currentScore = Number(prompt("Current Score: "));
+    let targetScore = Number(prompt("Desired Score: "));
+
+    console.log("Script start");
+
+    for (let i = currentScore + 1; i <= targetScore; i++) {
+        payload["score"] = i;
+        payload["record"] = i;
+
+        try {
+            const response = await fetch(url, {
+                method: 'POST',
+                headers: { // idk why this is needed
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(payload) // object to JSON
+            });
+
+            if (response.ok) {
+                console.log(`Successfully updated score to: ${i}`);
+            } 
+            else {
+                console.log("Something wrong with DeltaMath");
+                break;
+            }
+        } 
+        catch (error) {
+            console.error("Something wrong with DeltaMath");
+            break;
+        }
+
+        await new Promise(resolve => setTimeout(resolve, 250));
+    }
+
+    console.log("Finished!");
+})();
